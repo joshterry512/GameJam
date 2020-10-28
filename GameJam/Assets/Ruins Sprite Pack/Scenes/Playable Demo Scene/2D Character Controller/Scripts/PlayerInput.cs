263cd8a7bb73c9467ecece5772ea3cccfb851133
Matthew Vu <matthew.svu@gmail.com>
2020-10-28 03:28:16 -0500

    Made dash better

--- GameJam/Assets/Ruins Sprite Pack/Scenes/Playable Demo Scene/2D Character Controller/Scripts/PlayerInput.cs
+++ GameJam/Assets/Ruins Sprite Pack/Scenes/Playable Demo Scene/2D Character Controller/Scripts/PlayerInput.cs
@@ -3,18 +3,23 @@ using System.Collections;
 
 [RequireComponent (typeof (Player))]
 public class PlayerInput : MonoBehaviour {
-
+	private Rigidbody2D rigidBody;
+    public float dashSpeed;
+    private float dashTime;
+    public float startDashTime;
+	 private int direction;
 	Player player;
 	public Animator animator;
-	
 	void Start () {
 		player = GetComponent<Player> ();
+		rigidBody = GetComponent<Rigidbody2D>();
+        dashTime = startDashTime;
 	}
 
 	void Update () {
 		Vector2 directionalInput = new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"));
 		player.SetDirectionalInput (directionalInput);
-
+		Dash();
 		if (Input.GetKeyDown (KeyCode.Z)) {
 			player.OnJumpInputDown (1);
 		}
@@ -25,6 +30,7 @@ public class PlayerInput : MonoBehaviour {
         {
               Attack();
         }
+
 	}
 	void Attack() {
         // play an attack animation
@@ -32,4 +38,34 @@ public class PlayerInput : MonoBehaviour {
         // Detect enemies in range of attack
         // damage the enemies
     }
+
+	void Dash() {
+		if(direction == 0) {
+            if(Input.GetKeyDown(KeyCode.LeftArrow)) {
+                direction = 1;
+            }
+            else if(Input.GetKeyDown(KeyCode.RightArrow)) {
+                direction = 2;
+            }
+        }
+        else {
+            if(dashTime <= 0) {
+				animator.SetBool("isDash", false);
+                direction = 0;
+                dashTime = startDashTime;
+                rigidBody.velocity = Vector2.zero;
+            }
+            else {
+                dashTime -= Time.deltaTime;
+                if(direction == 1 && Input.GetKeyDown(KeyCode.C)) {
+					animator.SetBool("isDash", true);
+                    rigidBody.velocity = Vector2.left * dashSpeed;
+                }
+                else if(direction == 2 && Input.GetKeyDown(KeyCode.C)) {
+					animator.SetBool("isDash", true);
+                    rigidBody.velocity = Vector2.right * dashSpeed;
+                }
+            }
+        }
+	}
 }
