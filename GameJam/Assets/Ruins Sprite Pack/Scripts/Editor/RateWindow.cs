318834bcc344edf29227de2e79a50e663bdda11c
rickyVvv <48536558+rickyVvv@users.noreply.github.com>
2020-10-30 02:40:24 -0700

    Revert "Level 2 and added prefabs"
    This reverts commit 50624880204d0a43e47ea5a477b7aaf8382091f7.

--- GameJam/Assets/Ruins Sprite Pack/Scripts/Editor/RateWindow.cs
+++ GameJam/Assets/Ruins Sprite Pack/Scripts/Editor/RateWindow.cs
@@ -4,6 +4,13 @@ using UnityEditor;
 [InitializeOnLoad]
 public class RateWindow : EditorWindow
 {
+    Texture2D banner;
+
+    void OnEnable()
+    {
+        banner = (Texture2D)Resources.Load("Rate Window", typeof(Texture2D));
+    }
+
     static RateWindow()
     {
         EditorApplication.update += RunOnce;
@@ -13,17 +20,19 @@ public class RateWindow : EditorWindow
     {
         EditorApplication.update -= RunOnce;
 
-        bool dontShow = EditorPrefs.GetBool("DontShowRateWindow");
+        bool dontShow = EditorPrefs.GetBool("DontShowRateWindow_RuinsSpritePack");
 
         if (!Application.isPlaying)
             if (!dontShow)
                 Init();
     }
 
-    [MenuItem("Forest Parallax Backgrounds/RateWindow")]
+    [MenuItem("Help/Ruins Sprite Pack/Rate Window")]
     public static RateWindow Init()
     {
         RateWindow window = EditorWindow.GetWindow<RateWindow>(true, "Thank you!", true);
+        window.minSize = new Vector2(400, 420);
+        window.maxSize = new Vector2(400, 420);
         return window;
     }
 
@@ -42,8 +51,14 @@ public class RateWindow : EditorWindow
         bodyStyle.alignment = TextAnchor.UpperLeft;
         bodyStyle.richText = true;
 
-        string titleText = "Hey! Thanks for acquiring the Forest Parallax Backgrounds!";
-        string bodyText = "Just wanted to ask you to spare a few seconds to give a rating to the Forest Parallax Backgrounds on the store. It will help tremendously to support further development of the package.\n\n" +
+        GUIStyle bannerStyle = new GUIStyle();
+        bannerStyle.fixedWidth = 400;
+        bannerStyle.fixedHeight = 200;
+
+        GUILayout.Box(banner, bannerStyle);
+
+        string titleText = "Hey! Thanks for acquiring the Ruins Sprite Pack!";
+        string bodyText = "Just wanted to ask you to spare a few seconds to give a rating to the Ruins Sprite Pack on the store. It will help tremendously to support further development of the package.\n\n" +
                           "Thanks again! \n";
 
         GUILayout.Label(titleText, titleStyle);
@@ -56,9 +71,9 @@ public class RateWindow : EditorWindow
 
         Color col = GUI.color;
         GUI.backgroundColor *= Color.green;
-        if (GUILayout.Button("Rate Forest Parallax Backgrounds!", GUILayout.Height(30), GUILayout.MaxWidth(300)))
+        if (GUILayout.Button("Rate Ruins Sprite Pack!", GUILayout.Height(30), GUILayout.MaxWidth(300)))
         {
-            Application.OpenURL("http://u3d.as/YMT");
+            Application.OpenURL("http://u3d.as/1Day");
         }
         GUI.backgroundColor = col;
 
@@ -77,7 +92,7 @@ public class RateWindow : EditorWindow
 
     void SavePreference()
     {
-        EditorPrefs.SetBool("DontShowRateWindow", dontShowAgain);
+        EditorPrefs.SetBool("DontShowRateWindow_RuinsSpritePack", dontShowAgain);
     }
 
 }
