6b4ad6388f0765c90ebc24629786ab8dbcf460a8
joshterry512 <70680178+joshterry512@users.noreply.github.com>
2020-10-30 19:55:46 -0500

    Plezz work

---
+++ GameJam/Assets/Pause2.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.Hosting;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause2 : MonoBehaviour
+{
+    public static bool GameIsPause = false;
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPause)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPause = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPause = true;
+    }
+
+    public void GameExit()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
+    }
+}
